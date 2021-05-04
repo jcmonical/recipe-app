f791ab97f20a3a9ed6fc5990e75aeed348556827
jcmonical <82777165+jcmonical@users.noreply.github.com>
2021-05-03 19:03:56 -0500

    test

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+@Output() featureSelect = new EventEmitter<string>();
 
+  onSelect(feature: string) {
+    debugger;
+    this.featureSelect.emit(feature);
+}
 }
